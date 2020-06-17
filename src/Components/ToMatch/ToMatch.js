df3b3e88a926f98ae399a78dfc170236b48a199f
Mark-of-sky <48365383+Mark-of-sky@users.noreply.github.com>
2020-06-17 02:47:54 +0200

    Add files via upload

--- src/Components/ToMatch/ToMatch.js
+++ src/Components/ToMatch/ToMatch.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import { DragPreviewImage, useDrag } from 'react-dnd'
+import React, {useState} from 'react';
+import { DragPreviewImage, useDrop } from 'react-dnd'
 //import { ItemTypes } from './ItemTypes'
 import './ToMatch.css'
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
 
-const ToMatch = ({label}) => {
-   
+const ItemTypes = {
+  ANSWER: 'answer',
+}
+
+const ToMatch = ({onDrop, label}) => {
+    const [value, setValue] = useState();
+    const [{isOver, canDrop }, drop] = useDrop({
+        accept: ItemTypes.ANSWER,
+        drop: (item, monitor)=> { setValue(item.name)},
+        collect: monitor =>({
+          isOver: !!monitor.isOver()
+        })
+    })
+
+
+
       return (
 
-            <div className="tomatch">
+            <div  className="tomatch">
               <p>{label}</p>
-              <div className="drop"></div>
+              <div 
+              ref={drop} 
+              className="drop"
+              style={{
+                backgroundColor: isOver ? 'blue' : 'yellow'
+              }}
+              >
+               {value}
+              </div>
             </div>
 
    
     )
   };
-export default ToMatch
+
+  export default ToMatch;
+
